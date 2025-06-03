81f9e8bedde1c51c0206d1f71e258656eee8702f
4294967295 <4294967295@snu.ac.kr>
2025-06-03 10:35:55 +0900

    change pi

--- polar_sin.js
+++ polar_sin.js
@@ -1,13 +1,13 @@
 a=effect("phase")("Slider");
 b=1 / effect("amp")("Slider");
-d=effect("freq")("Slider")/3.6;
+d=effect("freq")("Slider");
 R=effect("resolution")("Slider");
 c=effect("height")("Slider")
 t= effect("theta range")("Slider");
 P=[];
 
 for (i=0;i<R*t;i++){
-P.push([c * Math.cos(i/R) + (Math.cos(i/R) * Math.sin(d * (a + (22/7) * (i/R))))/b, c * Math.sin(i/R) + (Math.sin(i/R) * Math.sin(d * (a + (22/7) * (i/R))))/b]);
+P.push([c * Math.cos(i/R) + (Math.cos(i/R) * Math.sin(d * (a + (Math.PI) * (i/R))))/b, c * Math.sin(i/R) + (Math.sin(i/R) * Math.sin(d * (a + (Math.PI) * (i/R))))/b]);
 }
 
 createPath(P,[],[], false);
